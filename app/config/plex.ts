162bb8edde513bfa813d2e803bf0aedb451a79e0
Maxime Tournier <speedrun.fra@gmail.com>
2026-01-26 20:00:03 -0500

    fix plex params server

--- app/config/plex.ts
+++ app/config/plex.ts
@@ -4,9 +4,6 @@ export const plexToken = process.env.PLEX_SERVER_TOKEN!
 export const plexClient = process.env.PLEX_CLIENT_IDENTIFIER!
 
 const plexAPI = new PlexAPI({
-  protocol: process.env.PLEX_SERVER_PROTOCOL as "http" | "https",
-  ipDescription: process.env.PLEX_SERVER_IP,
-  port: process.env.PLEX_SERVER_PORT,
   fullServerUrl: process.env.PLEX_SERVER_URL,
 })
 
--- dist/app/config/plex.js
+++ dist/app/config/plex.js
@@ -2,9 +2,6 @@ import { PlexAPI } from "@lukehagar/plexjs";
 export const plexToken = process.env.PLEX_SERVER_TOKEN;
 export const plexClient = process.env.PLEX_CLIENT_IDENTIFIER;
 const plexAPI = new PlexAPI({
-    protocol: process.env.PLEX_SERVER_PROTOCOL,
-    ipDescription: process.env.PLEX_SERVER_IP,
-    port: process.env.PLEX_SERVER_PORT,
     fullServerUrl: process.env.PLEX_SERVER_URL,
 });
 export default plexAPI;
